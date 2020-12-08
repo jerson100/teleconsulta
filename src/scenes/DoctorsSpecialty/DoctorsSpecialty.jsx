a2cd59a5ef3b1634b4f5e8fbfa7ee7dcfea6d158
jerson100 <juamkoo@gmail.com>
2020-12-08 09:17:32 -0500

    Ahora podemos buscar una especialidad

--- src/scenes/DoctorsSpecialty/DoctorsSpecialty.jsx
+++ src/scenes/DoctorsSpecialty/DoctorsSpecialty.jsx
@@ -1,5 +1,7 @@
 import { Col, Row } from "antd";
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { Helmet } from "react-helmet";
+import { useLocation } from "react-router-dom";
 import Container from "../../components/common/Container";
 import DoctorSpecialty from "./components/DoctorSpecialty";
 import DoctorsSpecialtySearch from "./components/DoctorsSpecialtySearch";
@@ -33,24 +35,51 @@ const especialidades = [
 ];
 
 const DoctorsSpecialty = () => {
+  const { search } = useLocation();
+  const [specialties, setSpecialties] = useState([]);
+  //   console.log(search);
+
+  useEffect(() => {
+    //llamada a la api
+    setSpecialties(especialidades);
+  }, []);
+
+  const daatSearch = useMemo(() => {
+    const s = search.replace("?q=", "")?.toLowerCase();
+    // console.log(s);
+    if (s === "") return specialties;
+    return specialties.filter(
+      (ss) => ss.especialty.toLowerCase().indexOf(s) >= 0
+    );
+  }, [search, specialties]);
+
   return (
-    <div className="doctors-specialty">
-      <Container>
-        <DoctorsSpecialtySearch />
-        <Row
-          gutter={[
-            { md: 16, xl: 32 },
-            { xs: 16, md: 16, xl: 32 },
-          ]}
-        >
-          {especialidades.map((e, i) => (
-            <Col xs={{ span: 24 }} md={{ span: 12 }} xl={{ span: 8 }} key={i}>
-              <DoctorSpecialty especialty={e.especialty} image={e.image} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Nuestra empresa cuenta con una variedad de especialidades."
+        ></meta>
+        <title>Especialidades | teleconsulta</title>
+      </Helmet>
+      <div className="doctors-specialty">
+        <Container>
+          <DoctorsSpecialtySearch search={search} />
+          <Row
+            gutter={[
+              { md: 16, xl: 32 },
+              { xs: 16, md: 16, xl: 32 },
+            ]}
+          >
+            {daatSearch.map((e, i) => (
+              <Col xs={{ span: 24 }} md={{ span: 12 }} xl={{ span: 8 }} key={i}>
+                <DoctorSpecialty especialty={e.especialty} image={e.image} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 };
 
--- src/scenes/DoctorsSpecialty/components/DoctorsSpecialtySearch/DoctorsSpecialtySearch.jsx
+++ src/scenes/DoctorsSpecialty/components/DoctorsSpecialtySearch/DoctorsSpecialtySearch.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Form, Input, Typography } from "antd";
 import "./doctorsSpecialtySearch.scss";
+import { useHistory } from "react-router-dom";
 const { Title } = Typography;
 
-const DoctorsSpecialtySearch = () => {
+const DoctorsSpecialtySearch = ({ search }) => {
+  const { push } = useHistory();
+  const handleSubmit = (e) => {
+    // console.log(e);
+    push(`/especialidades${e.specialty === "" ? "" : `?q=${e.specialty}`}`);
+  };
+  //   console.log(search);
   return (
     <div className="doctors-specialty__search">
       <Title level={2}>Buscar: </Title>
-      <Form>
-        <Form.Item>
+      <Form onFinish={handleSubmit}>
+        <Form.Item name="specialty" initialValue={search.replace("?q=", "")}>
           <Input />
         </Form.Item>
       </Form>
@@ -16,4 +23,4 @@ const DoctorsSpecialtySearch = () => {
   );
 };
 
-export default DoctorsSpecialtySearch;
+export default React.memo(DoctorsSpecialtySearch);
