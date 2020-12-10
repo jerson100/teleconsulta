a66f4d44a8530fa9cb47ca549b9a95e9f5ed316f
jerson100 <juamkoo@gmail.com>
2020-12-10 17:51:16 -0500

    Se arreglo el button done del step

--- src/components/common/JeSteps/components/JeStepsButtonDone/JeStepsButtonDone.jsx
+++ src/components/common/JeSteps/components/JeStepsButtonDone/JeStepsButtonDone.jsx
@@ -1,14 +1,13 @@
 import { Button } from "antd";
 import React from "react";
+import useJeSelectCardContext from "../../../JeSelectCard/hooks/useJeSelectCard";
 
 const JeStepsButtonDone = () => {
+  const { current, steps } = useJeSelectCardContext();
   return (
     <>
       {current === steps.length - 1 && (
-        <Button
-          type="primary"
-          onClick={() => message.success("Processing complete!")}
-        >
+        <Button type="primary" onClick={() => console.log("done!...")}>
           Done
         </Button>
       )}
